f615ccaaaf99a7930b65a42b923b2a2987209f63
Toshika-02 <105552050+Toshika-02@users.noreply.github.com>
2024-04-23 20:28:04 +0530

    finish

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,1 +1,15 @@
+import React from "react";
 
+const Editor = ({ markdown, onMarkdownChange }) => {
+  const handleChange = (event) => {
+    onMarkdownChange(event.target.value);
+  };
+
+  return (
+    <div className="editor">
+      <textarea value={markdown} onChange={handleChange} />
+    </div>
+  );
+};
+
+export default Editor;
